07ea45fc6d17f557d94165cfee2772f8a4866765
Filipe Granacha <filipegranacha@hotmail.com>
2018-08-01 17:26:20 +0100

    added extra test

--- Granacha.Functions.UnitTest/FibonacciTest.cs
+++ Granacha.Functions.UnitTest/FibonacciTest.cs
@@ -20,5 +20,12 @@ namespace Granacha.Functions.UnitTest
             Assert.That(elements.Count, Is.EqualTo(5));
             Assert.IsTrue(elements.Any(element => element.Equals(Fibonacci.Calculate(elements.IndexOf(element)))));
         }
+
+        [Test]
+        public void When_I_Call_GetNElements_With_Zero()
+        {
+            var elements = Fibonacci.GetFirstNElements(0);
+            Assert.IsFalse(elements.Any());
+        }
     }
 }
